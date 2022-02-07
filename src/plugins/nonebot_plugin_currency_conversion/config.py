d2bbfeaee5bd9d8caafe443aade391e8b9d3e730
summerkirakira <zhenghongpei@gmail.com>
2022-02-07 20:33:25 +0000

    添加正则词图结合功能

--- src/plugins/nonebot_plugin_currency_conversion/config.py
+++ src/plugins/nonebot_plugin_currency_conversion/config.py
@@ -1,4 +1,17 @@
 from pydantic import BaseSettings
+import os
+import yaml
+current_folder = os.path.dirname(__file__)
+
+
+def get_config() -> dict:
+    with open(os.path.join(current_folder, 'config.yaml'), "r") as file:
+        return yaml.safe_load(file.read())
+
+
+def save_config_to_yaml(server_info):
+    with open(os.path.join(current_folder, 'config.yaml'), 'w') as f:
+        f.write(yaml.dump(server_info, allow_unicode=True, sort_keys=False))
 
 
 class Config(BaseSettings):
